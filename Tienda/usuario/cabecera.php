43fa6ec155f67a64529eb76eafa8c161847a3319
Jhon <yhonnyhst@gmail.com>
2018-11-28 15:21:29 -0500

    iconos

--- Tienda/usuario/cabecera.php
+++ Tienda/usuario/cabecera.php
@@ -7,6 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/tienda.png">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Usuario</title>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
